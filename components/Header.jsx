0a9b92fb09c3f8ec080f6737beca4216aecf602a
Oli <oliverrsaxon@gmail.com>
2022-02-26 15:40:37 +0000

    fixing issues again

--- components/Header.jsx
+++ components/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
       </nav>
       <header className="bg-gray-800 shadow">
         <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-50">{user}</h1>
+          <h1 className="text-3xl font-bold text-gray-50"></h1>
         </div>
       </header>
     </>
